66e8e574ef8d643a482f46de41ff7f9e40601944
h28i27 <h28i27@ed.cc.suzuka-ct.ac.jp>
2024-08-18 21:03:47 +0900

    Initial Commit

---
+++ migraine/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  migraine
+//
+//  Created by 中川悠 on 2024/08/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ migraine/migraineApp.swift
@@ -0,0 +1,17 @@
+//
+//  migraineApp.swift
+//  migraine
+//
+//  Created by 中川悠 on 2024/08/18.
+//
+
+import SwiftUI
+
+@main
+struct migraineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
